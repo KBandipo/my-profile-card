94038b78173bb854d528fbb27937d96ce331a76e
KBandipo~ <keneebandipo@gmail.com>
2024-02-06 13:34:47 +0100

    Render the SkillList items

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import "./index.css";
 
+const skills = [
+  { skill: "HTML", level: "advance", color: "#55FF00" },
+  { skill: "CSS", level: "advance", color: "#FF3B00" },
+  { skill: "JavaScript", level: "intermediate", color: "#E85F90" },
+  { skill: "Tailwind", level: "advance", color: "#DA2399" },
+  { skill: "Bootstrap", level: "intermediate", color: "#44dddd" },
+  { skill: "React", level: "intermediate", color: "#F0DAF8" },
+  { skill: "Git and Github", level: "intermediate", color: "#e84f99" },
+  { skill: "Web Development", level: "intermediate", color: "#55FFCC" },
+  { skill: "SASS", level: "beginner", color: "#DAdddd" },
+  { skill: "QA Testing", level: "intermediate", color: "#FF3F" },
+  { skill: "SQL", level: "intermediate", color: "#66ff" },
+  { skill: "Postman", level: "intermediate", color: "#99aa55" },
+  { skill: "Gira", level: "intermediate", color: "#44dd" },
+];
+
 function App() {
   return (
     <div className="card">
@@ -25,7 +41,7 @@ function Avatar() {
 function Intro() {
   return (
     <div>
-      <h1>Bandipo, Kehinde Olushola</h1>
+      <h1>Bandipo, Kehinde</h1>
       <p>
         I am a versatile professional with expertise in both Frontend
         Development and Quality Assurance Engineering. My adeptness in
@@ -46,30 +62,35 @@ function Intro() {
 // SkillList
 function SkillList() {
   return (
+    // <div className="skill-list">
+    //   <Skill skill="HTML" emoji="👌" color="#55FF00" />
+    //   <Skill skill="CSS" emoji="👌" color="#FF3B00" />
+    //   <Skill skill="JavaScript" emoji="👌" color="#E85F90" />
+    //   <Skill skill="React" emoji="👌" color="#DA2399" />
+    //   <Skill skill="Tailwind" emoji="👌" color="#44dddd" />
+    //   <Skill skill="Bootstrap" emoji="🌱" color="#F0DAF8" />
+    //   <Skill skill="Web Development" emoji="👍" color="#e84f99" />
+    //   <Skill skill="Git and Github" emoji="👍" color="#55FFCC" />
+    //   <Skill skill="SASS" emoji="🌱" color="#DAdddd" />
+    //   <Skill skill="QA Testing" emoji="👍" color="#FF3F" />
+    //   <Skill skill="SQL" emoji="👍" color="#66ff" />
+    //   <Skill skill="Postman" emoji="👍" color="#99aa55" />
+    //   <Skill skill="Gira" emoji="👍" color="#44dd" />
+    // </div>
     <div className="skill-list">
-      <Skill skill="HTML" emoji="👌" color="#55FF00" />
-      <Skill skill="CSS" emoji="👌" color="#FF3B00" />
-      <Skill skill="JavaScript" emoji="👌" color="#E85F90" />
-      <Skill skill="React" emoji="👌" color="#DA2399" />
-      <Skill skill="Tailwind" emoji="👌" color="#44dddd" />
-      <Skill skill="Bootstrap" emoji="🌱" color="#F0DAF8" />
-      <Skill skill="Web Development" emoji="👍" color="#e84f99" />
-      <Skill skill="Git and Github" emoji="👍" color="#55FFCC" />
-      <Skill skill="SASS" emoji="🌱" color="#DAdddd" />
-      <Skill skill="QA Testing" emoji="👍" color="#FF3F" />
-      <Skill skill="SQL" emoji="👍" color="#66ff" />
-      <Skill skill="Postman" emoji="👍" color="#99aa55" />
-      <Skill skill="Gira" emoji="👍" color="#44dd" />
+      {skills.map((skill) => (
+        <Skill skillObj={skill} />
+      ))}
     </div>
   );
 }
 
 // Skill is component of SkillList
-function Skill({ skill, emoji, color }) {
+function Skill({ skillObj }) {
   return (
-    <div className="skill" style={{ backgroundColor: color }}>
-      <span>{skill}</span>
-      <span>{emoji}</span>
+    <div className="skill" style={{ backgroundColor: skillObj.color }}>
+      <span>Obj{skillObj.skill}</span>
+      <span>{skillObj.emoji}</span>
     </div>
   );
 }
